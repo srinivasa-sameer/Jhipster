225f6b2f224e8bf7a2cf15b850d810c0b008b7d7
Srinivasa Sameer Addepalli <srinivasaddepalli13@gmail.com>
2021-05-04 18:32:56 +0530

    Initial version of poc generated by JHipster-7.0.1

---
+++ src/main/java/com/jhipster/poc/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.jhipster.poc.repository;
+
+import com.jhipster.poc.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
